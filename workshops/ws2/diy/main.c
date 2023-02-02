00f1db8185d52ed9346031e6da68dc85da9979d5
Irish Banga <ibanga1@myseneca.ca>
2023-02-01 22:32:00 -0500

    w

--- workshops/ws2/diy/main.c
+++ workshops/ws2/diy/main.c
@@ -2,7 +2,7 @@
 Name: Irish Banga
 Email: ibanga1@myseneca.ca
 Student ID: 112435227
-Competion Date: 2023-01-20
+Competion Date: 2023-02-01
 
 I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 */
@@ -10,13 +10,10 @@ I have done all the coding by myself and only copied the code that my professor 
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
-
 void rectangle(char leftTop, char top, char topRight,
     char right, char rightButtom, char buttom,
     char buttomLeft, char left, char fill,
     int width, int height);
-
-
 
 int main(void)
 {

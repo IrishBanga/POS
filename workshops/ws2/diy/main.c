ce9d10f4d834b058092b3de338b72a438ab171af
Irish Banga <ibanga1@myseneca.ca>
2023-02-01 22:00:55 -0500

    ws 2

---
+++ workshops/ws2/diy/main.c
@@ -0,0 +1,14 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+Competion Date: 2023-01-20
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+// this is empty file created due to submission error 
+// the instructions specified creating the rectangle function in draw.c file

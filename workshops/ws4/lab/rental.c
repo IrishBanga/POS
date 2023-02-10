2f20f4151fabeb7f2c8aad5ecd16dadf1cb39eae
Irish Banga <ibanga1@myseneca.ca>
2023-02-10 15:21:27 -0500

    WS4

---
+++ workshops/ws4/lab/rental.c
@@ -0,0 +1,125 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+Competion Date: 2023-02-10
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#include "utils.h"
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+char getSingleChar(void);
+int yes(void);
+
+void menu(void)
+{
+    printf("%-14s%-11s%s\n", "Equipment", "Price", "SKU");
+	line('-', 28);
+    printf("%-14s%-11s%s\n", "Skis","12.30","101");
+    printf("%-14s%-11s%s\n", "Boots", "10.45", "102");
+    printf("%-14s%-11s%s\n", "Poles", "2.99", "203");
+    printf("%-14s%-11s%s\n", "Helmet", "3.99", "204");
+    printf("%-14s%-11s%s\n", "Winter Pants", "14.30", "305");
+    printf("%-14s%-11s%s\n", "Winter Jacket", "20.55", "306");
+    line('-', 28);
+}
+
+
+
+double rent(int sku)
+{
+    double price = 0.0;
+    switch (sku)
+    {
+    case 101:
+        price = 12.30;
+        printf("Skis ............. %5.2lf$\n",price);
+        break;
+    case 102:
+        price = 10.45;
+        printf("Boots ............ %5.2lf$\n", price);
+        break;
+    case 203:
+        price = 2.99;
+        printf("Poles ............ %5.2lf$\n", price);
+        break;
+    case 204:
+        price = 3.99;
+        printf("Helmet ........... %5.2lf$\n", price);
+        break;
+    case 305:
+        price = 14.30;
+        printf("Winter Pants ..... %5.2lf$\n", price);
+        break;
+    case 306:
+        price = 20.55;
+        printf("Winter Jacket .... %5.2lf$\n", price);
+        break;
+    default:
+        printf("Invalid Selection!\n");
+        
+    }
+    return price;
+}
+
+
+void rental(void)
+{
+    int sku;
+    int done = 0;
+    double total = 0.0;
+    do
+    {
+        printf("Enter Sku or 0 to exit: ");
+        sku = getInt();
+        if (sku != 0)
+            total = total + rent(sku);
+        else
+            done = 1;
+    } 
+    while (!done);
+        line('-', 28);
+        printf("%-19s%5.2lf\n","Total price:",total);
+        printf("%-19s%5.2lf\n","Tax",total*0.13);
+        printf("%-19s%5.2lf$\n","Total + Tax:",total*1.13);
+        line('*', 28);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int yes(void) 
+{
+    int res;
+    char resp;
+    printf("(Y)es or (N)o: ");
+    resp = getSingleChar();
+    if (resp == 'Y' || resp == 'y') {
+        res = 1;
+    }
+    else {
+        res = 0;
+    }
+    return res;
+}
+char getSingleChar(void) 
+{
+    char ch = getchar();
+    flushKey();
+    return ch;
+}
---
+++ workshops/ws4/lab/utils.c
@@ -0,0 +1,130 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+Competion Date: 2023-02-08
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void flushKey();
+
+int getInt()
+{
+    int value = 0;
+    int done = 0;
+    char check = 0;
+    while (!done) 
+    {
+        if (scanf("%d%c", &value, &check) == 2 && check == '\n')
+        {
+            done = 1;
+        }
+        else
+        { 
+            flushKey();
+            if (value == 0)
+            {
+                printf("Invalid Integer, try again: ");
+
+            }
+            else
+            {
+                printf("Enter only an integer, try again: ");
+            }
+        }
+    }
+    return value;
+}
+
+
+int getIntMM(int min, int max)
+{
+    int value;
+    int done = 0;
+    value = getInt();
+    while (!done)
+    {
+        if (value >= min && value <= max)
+        {
+            done = 1;
+        }
+        else
+        {
+            printf("[%d<=Number<=%d], try again: ",min,max);
+            value = getInt();
+        }
+    }
+    return value;
+}
+
+double getDouble(void)
+{
+    double dvalue = 0.0;
+    int done = 0;
+    char check = 0;
+    while (!done) 
+    {
+        if (scanf("%lf%c", &dvalue, &check) == 2 && check == '\n')
+        {
+            done = 1;
+        }
+        else
+        { 
+            flushKey();
+            if (dvalue == 0.0)
+            {
+                printf("Invalid Double, try again: ");
+
+            }
+            else
+            {
+                printf("Enter only a Double, try again: ");
+            }
+        }
+    }
+    return dvalue;
+}
+
+double getDoubleMM(double min, double max)
+{
+    double dvalue;
+    int done = 0;
+    dvalue = getDouble();
+    while (!done)
+    {
+        if (dvalue >= min && dvalue <= max)
+        {
+            done = 1;
+        }
+        else
+        {
+            printf("[%.3lf<=Number<=%.3lf], try again: ",min,max);
+            dvalue = getDouble();
+        }
+    }
+    return dvalue;
+}
+
+void line(char fill, int length)
+{
+    int i = 0;
+    while (i < length)
+    {
+        putchar(fill);
+        i++;
+    }
+    putchar('\n');
+}
+
+void flushKey()
+{
+    char ch = 'x';
+    while (ch != '\n')
+    {
+        ch = getchar();
+    }
+}

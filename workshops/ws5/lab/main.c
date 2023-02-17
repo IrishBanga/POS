fa163ba9fb3a45ef2dde423edcb6a927b31965e8
Irish Banga <ibanga1@myseneca.ca>
2023-02-17 14:32:43 -0500

    ws5 lab draft

---
+++ workshops/ws5/lab/main.c
@@ -0,0 +1,19 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+Completion Date: 2023-02-17
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#include "classList.h"
+int main() {
+	int stno[4] = { 12345, 23456, 345678, 45678 };
+	int mark[4] = { 55,66,44,88 };
+	printReport("IPC144NBB", stno, mark, 4);
+	return 0;
+}

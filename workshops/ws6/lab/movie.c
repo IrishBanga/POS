ec86b83c618976726d3bfac04e4e4c81bbbcb9fe
Irish Banga <ibanga1@myseneca.ca>
2023-03-24 14:10:18 -0400

    WORKSHOP 6

---
+++ workshops/ws6/lab/movie.c
@@ -0,0 +1,65 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+Completion Date: 2023-03-24
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "movie.h"
+#include "utils.h"
+
+int loadMovie(struct Movie* mptr, FILE* fptr)
+{
+	int success=0;
+	if (fscanf(fptr, "%[^\t]\t%d\t%[^\t]\t%d\t%[^\t]\t%f\n", mptr->title, &mptr->year, mptr->rating, &mptr->duration, mptr->genre, &mptr->crating) == 6)
+	{
+		success=1;
+	}
+	return success;
+}
+
+void printInWidth(const char str[], int width)
+{
+	putchar('|');
+	int i=0;
+	int check = 0;
+	check=printf("%-*.*s",width,width, str);
+	/*for (i = 0; i < width-check; i++)
+	{
+		putchar(' ');
+	}*/
+	putchar('|');
+	
+}
+
+void list(const struct Movie* mptr, int row)
+{
+	//printf("%s\t%d\t%s\t%d\t%s\t%.1f\n", mptr->title, mptr->year, mptr->rating, mptr->duration, mptr->genre, mptr->crating);
+	printf("|%4d ",row);
+	printInWidth(mptr->title, 20);
+	printf(" %d ", mptr->year);
+	printf("| %5s |",mptr->rating);
+	printf(" %d:%0.2d ", mptr->duration/60, mptr->duration%60);
+	printInWidth(mptr->genre, 25);
+	printf(" %.1f |", mptr->crating);
+	putchar('\n');
+}
+
+void display(const struct Movie* m)
+{
+	printf("Title: %s\n", m->title);
+	printf("Year: %d\n", m->year);
+	printf("Rating: %s\n", m->rating);
+	printf("Duration: %d:%0.2d ", m->duration / 60, m->duration % 60);
+	printf("Genres: %s\n", m->genre);
+	printf("Consumer Rating: %.1f\n", m->crating);
+}
+
+const char* getMovieTitle(const struct Movie* mptr)
+{
+	return mptr->title;
+}
---
+++ workshops/ws6/lab/utils.c
@@ -0,0 +1,172 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+Completion Date: 2023-02-08
+Last Modified : 2023-03-24
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "utils.h"
+
+int getInt()
+{
+    int value = 0;
+    int done = 0;
+    char check = 0;
+    while (!done)
+    {
+        if (scanf("%d%c", &value, &check) == 2 && check == '\n')
+        {
+            done = 1;
+        }
+        else
+        {
+            flushKey();
+            if (value == 0)
+            {
+                printf("Invalid Integer, try again: ");
+
+            }
+            else
+            {
+                printf("Enter only an integer, try again: ");
+            }
+        }
+    }
+    return value;
+}
+
+
+int getIntMM(int min, int max)
+{
+    int value;
+    int done = 0;
+    value = getInt();
+    while (!done)
+    {
+        if (value >= min && value <= max)
+        {
+            done = 1;
+        }
+        else
+        {
+            printf("[%d<=Number<=%d], try again: ", min, max);
+            value = getInt();
+        }
+    }
+    return value;
+}
+
+double getDouble(void)
+{
+    double dvalue = 0.0;
+    int done = 0;
+    char check = 0;
+    while (!done)
+    {
+        if (scanf("%lf%c", &dvalue, &check) == 2 && check == '\n')
+        {
+            done = 1;
+        }
+        else
+        {
+            flushKey();
+            if (dvalue == 0.0)
+            {
+                printf("Invalid Double, try again: ");
+
+            }
+            else
+            {
+                printf("Enter only a Double, try again: ");
+            }
+        }
+    }
+    return dvalue;
+}
+
+double getDoubleMM(double min, double max)
+{
+    double dvalue;
+    int done = 0;
+    dvalue = getDouble();
+    while (!done)
+    {
+        if (dvalue >= min && dvalue <= max)
+        {
+            done = 1;
+        }
+        else
+        {
+            printf("[%.3lf<=Number<=%.3lf], try again: ", min, max);
+            dvalue = getDouble();
+        }
+    }
+    return dvalue;
+}
+
+void line(char fill, int length)
+{
+    int i = 0;
+    while (i < length)
+    {
+        putchar(fill);
+        i++;
+    }
+    putchar('\n');
+}
+
+void flushKey()
+{
+    char ch = 'x';
+    while (ch != '\n')
+    {
+        ch = getchar();
+    }
+}
+
+int yes(void)
+{
+    int res;
+    char resp;
+    printf("(Y)es or (N)o: ");
+    resp = getSingleChar();
+    if (resp == 'Y' || resp == 'y') {
+        res = 1;
+    }
+    else {
+        res = 0;
+    }
+    return res;
+}
+
+char getSingleChar(void)
+{
+    char ch = getchar();
+    flushKey();
+    return ch;
+}
+
+int getMMInt(int min, int max, const char valueName[])
+{
+    int value;
+    int done = 0;
+    value = getInt();
+    while (!done)
+    {
+        if (value >= min && value <= max)
+        {
+            done = 1;
+        }
+        else
+        {
+            printf("[%d<=%s<=%d], try again: ", min, valueName, max);
+            value = getInt();
+        }
+    }
+    return value;
+}
